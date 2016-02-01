0dfa170173ad87034a2faa1d37278b7f3c19a2f3
Manny Gimond <mgimond@colby.edu>
2016-02-01 14:08:05 -0500

    Added Common.R template. Continued with edits.

---
+++ libs/Common.R
@@ -0,0 +1,11 @@
+knitr::opts_chunk$set(
+  comment = NA,
+  message = FALSE,
+  tidy = FALSE,
+  cache = FALSE,
+  warning = FALSE,
+  encoding = "UTF-8",
+  fig.show='hold')
+
+#knitr::opts_knit$set(list(width = 140))
+options(width=120)
