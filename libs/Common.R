75f99865bcc250e47d34d2f5d495660e9f716210
Manny Gimond <mgimond@users.noreply.github.com>
2020-01-31 15:16:20 -0500

    re-knitted with R 3.6.2

--- libs/Common.R
+++ libs/Common.R
@@ -14,4 +14,40 @@ knitr::knit_hooks$set(small.mar = function(before, options, envir) {
 })
 
 #knitr::opts_knit$set(list(width = 140))
+knitr::clean_cache()
+
+# Set console width
 options(width=120)
+
+
+# Function that returns package versions
+pkg_ver <- function(x){
+  library(kableExtra, verbose = FALSE)
+  library(dplyr, verbose = FALSE, quietly = TRUE, warn.conflicts = FALSE)
+  ver.lst <- lapply(x, FUN = function(x) as.character(packageVersion(x)))
+  names(ver.lst) <- x
+  ver.df <- as.data.frame(ver.lst)
+  out <- ver.df %>% 
+    kable(format = "html", escape = F) %>%
+    kable_styling(full_width = F, position = "left") %>%
+    row_spec(0, background = "#dddddd", align = "c", color="#555555",
+             extra_css = "border: 1px solid white !important;
+             font-family: 'Source Code Pro', 'Open Sans';
+             padding:1px !important;
+             padding-left:4px !important;
+             padding-right:4px !important;
+             font-size: 0.8em;
+             border-radius: 5px;") %>% 
+    row_spec(1, background = "#FAE8E8", align = "c", color="darkred"  ,
+             extra_css = "border: 1px solid white;
+             font-family: 'Open Sans', Arial;
+             padding:1px !important;
+             padding-left:4px !important;
+             padding-right:4px !important;
+             font-size: 0.8em;
+             border-radius: 5px;")
+  detach("package:dplyr")
+  detach("package:kableExtra")
+  return(out)
+}
+
