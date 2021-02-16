686aa3f5f383603fe6c2e2ae91f0b5cf514d11dc
Manny Gimond <Manny@>
2021-02-15 20:21:29 -0500

    finalized new section

--- libs/Common.R
+++ libs/Common.R
@@ -51,3 +51,34 @@ pkg_ver <- function(x){
   return(out)
 }
 
+
+# Function that returns R versions
+R_ver <- function(x){
+  library(kableExtra, verbose = FALSE)
+  library(dplyr, verbose = FALSE, quietly = TRUE, warn.conflicts = FALSE)
+  version <- paste(R.version$major, R.version$minor, sep=".")
+  names(version) <- "R"
+  ver.df <- as.data.frame(version)
+  out <- ver.df %>% 
+    kable(format = "html", escape = F) %>%
+    kable_styling(full_width = F, position = "left") %>%
+    row_spec(0, background = "#dddddd", align = "c", color="#555555",
+             extra_css = "border: 1px solid white !important;
+             font-family: 'Source Code Pro', 'Open Sans';
+             padding:1px !important;
+             padding-left:4px !important;
+             padding-right:4px !important;
+             font-size: 0.8em;
+             border-radius: 5px;") %>% 
+    row_spec(1, background = "#FAE8E8", align = "c", color="darkred"  ,
+             extra_css = "border: 1px solid white;
+             font-family: 'Open Sans', Arial;
+             padding:1px !important;
+             padding-left:4px !important;
+             padding-right:4px !important;
+             font-size: 0.8em;
+             border-radius: 5px;")
+  detach("package:dplyr")
+  detach("package:kableExtra")
+  return(out)
+}
