afd01bbba94617bd4074d848bf79a7bfbf1e813f
Manny Gimond <Manny Gimond>
2025-01-18 19:56:05 -0500

    update

--- scratch.r
+++ scratch.r
@@ -10,5 +10,15 @@ set.seed(787)
 n <- 500
 x <- runif(n,-5,5) 
 y <- 11 + 0.2*x + 0.4 * x^2 + rnorm(n,0,abs(x+5)/5)
-df <- data.frame(x,y)
-OP<-par(mfrow=c(1,2));M <- eda_lm(df, x, y, poly=2);eda_sl(M);par(OP)
+df <- data.frame(X=x,Y=y)
+OP<-par(mfrow=c(1,2));M <- eda_lm(df, X, Y, poly=2);eda_sl(M;par(OP)
+
+
+OP<-par(mfrow=c(1,2));M <- eda_lm(df, X, Y, poly=2);
+eda_sl(M, type = "dependence");par(OP)
+
+
+cars <- MASS::Cars93
+M1 <- eda_lm(cars, EngineSize, Price, poly = 2)
+plot(M1)
+eda_sl(M1)
